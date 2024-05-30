2bacd4ef168f36c1107aa936339da6b858ed9edc
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-05-29 18:43:49 -0600

    Practica No.5: Joins

---
+++ DML-JoinsComicStore.sql
@@ -0,0 +1,29 @@
+
+
+-- Inner Join
+
+Select c.id_compra, cl.nombre AS CLIENTE, c.fecha_compra, c.total
+from Compras c
+JOIN Clientes cl ON c.id_cliente = cl.id_cliente; 
+
+
+-- LEFT 
+
+Select c.id_compra, cl.nombre AS CLIENTE, c.fecha_compra, c.total
+from Compras c
+LEFT JOIN Clientes cl ON c.id_cliente = cl.id_cliente; 
+
+
+-- RIGHT
+
+Select c.id_compra, cl.nombre AS CLIENTE, c.fecha_compra, c.total
+from Compras c
+RIGHT JOIN Clientes cl ON c.id_cliente = cl.id_cliente; 
+
+
+--FULL JOIN
+
+SELECT *
+FROM Clientes cl
+FULL JOIN Compras c
+ON c.id_cliente = cl.id_cliente; 
